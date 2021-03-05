9c6c5cac14d21c7be9236330d959451d82ed069e
Carlos Colnaghi <carlos.colnaghi@hotmail.com>
2021-03-05 15:24:38 -0300

    Criação do arquivo .gitignore

---
+++ Processamento de Imagens/Operacoes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Processamento_de_Imagens
+{
+     class Operacoes
+    {
+        public const string
+            binarizacao = "Binarização",
+            filtroMedia = "Filtro de Média";
+    }
+}
